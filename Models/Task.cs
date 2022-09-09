e82385d2b1ce785679c49e3b48a9ba344a9dab99
Frederick Lopez <mtester010@gmail.com>
2022-09-09 14:43:47 -0500

    Initial commit plus added task model

---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+
+        public Priority Priority { get; set; }
+
+    }
+}
