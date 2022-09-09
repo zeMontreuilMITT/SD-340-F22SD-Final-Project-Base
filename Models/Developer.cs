1715afb463004664e2de5c70d25a5dd945614d00
Shaun Senow <shaunsenow@student.mitt.ca>
2022-09-09 14:55:36 -0500

    Set up User and Developer

---
+++ Models/Developer.cs
@@ -0,0 +1,8 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public class Developer
+    {
+        public int TaskAmount { get; set; }
+        public List<Task> TaskList { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
