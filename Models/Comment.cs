1c97bca3c9ab0d371d1094f5826ca393bcfff0c9
Fulin <944799613@qq.com>
2022-09-12 11:24:16 -0500

    add Comment class

---
+++ Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public ApplicationUser CreatedBy { get; set; }
+
+        public ICollection<Ticket> Tickets = new HashSet<Ticket>();
+
+    }
+}
