4a2b94159a184b1640ce762bb6e9c4ba29b47c0c
Frederick Lopez <mtester010@gmail.com>
2022-09-12 13:51:46 -0500

    Add SeedData class for users and project

---
+++ Models/SeedData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SD_340_W22SD_Final_Project_Group6.Data;
+
+namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public static class SeedData
+    {
+        public async static Task Initialize(IServiceProvider serviceProvider)
+        {
+            ApplicationDbContext context
+                = new ApplicationDbContext(serviceProvider
+                .GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            RoleManager<IdentityRole> roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            UserManager<ApplicationUser> userManager =
+                serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            List<string> roles = new List<string>
+            {
+                "ProjectManager", "Developer"
+            };
+
+            if (!context.Roles.Any())
+            {
+                foreach (string role in roles)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Users.Any())
+            {
+                ApplicationUser seedProjectManagerUser = new ApplicationUser
+                {
+                    Email = "pm@jello.com",
+                    NormalizedEmail = "PM@JELLO.COM",
+                    UserName = "pm@jello.com",
+                    NormalizedUserName = "PM@JELLO.COM",
+                    Name = "Gus Jello",
+                    EmailConfirmed = true,
+                };
+
+                var password1 = new PasswordHasher<ApplicationUser>();
+                var hashed1 = password1.HashPassword(seedProjectManagerUser, "P@ssW0rd");
+                seedProjectManagerUser.PasswordHash = hashed1;
+
+                await userManager.CreateAsync(seedProjectManagerUser);
+                await userManager.AddToRoleAsync(seedProjectManagerUser, "ProjectManager");
+
+                ApplicationUser seedDeveloperUser1 = new ApplicationUser
+                {
+                    Email = "dev1@jello.com",
+                    NormalizedEmail = "DEV1@JELLO.COM",
+                    UserName = "dev1@jello.com",
+                    NormalizedUserName = "DEV1@JELLO.COM",
+                    EmailConfirmed = true,
+                    Name = "Pinkman Jello"
+                };
+
+                var password2 = new PasswordHasher<ApplicationUser>();
+                var hashed2 = password2.HashPassword(seedDeveloperUser1, "P@ssW0rd");
+                seedDeveloperUser1.PasswordHash = hashed2;
+
+                await userManager.CreateAsync(seedDeveloperUser1);
+                await userManager.AddToRoleAsync(seedDeveloperUser1, "GroupAdmin");
+
+
+                if (!context.Projects.Any())
+                {
+                    Project newProject = new Project
+                    {
+                        ProjectName = "Flavor Shots",
+                        CreatedBy = seedProjectManagerUser,
+                        AssignedTo = { seedProjectManagerUser, },
+                        Tickets = { },
+                    };
+
+                    Project newProject2 = new Project
+                    {
+                        ProjectName = "John Cena Endorsement",
+                        CreatedBy = seedProjectManagerUser,
+                        AssignedTo = { seedProjectManagerUser, },
+                        Tickets = { },
+                    };
+
+                    context.Add(newProject);
+                    context.Add(newProject2);
+                }
+
+                await context.SaveChangesAsync();
+            }
+
+
+        }
+    }
+}
