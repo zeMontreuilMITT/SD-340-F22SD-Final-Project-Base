551170609c6544713276f39494866d12f943c3f7
Shaun Senow <shaunsenow@student.mitt.ca>
2022-09-12 10:28:44 -0500

    Rebuilt the base user class

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SD_340_W22SD_Final_Project_Group6.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Ticket> tickets { get; set; } = new HashSet<Ticket>();
+        public ICollection<Project> projects { get; set; } = new HashSet<Project>();
+
+    }
+}
--- Models/Developer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.Models
-{
-    public class Developer
-    {
-        public int TaskAmount { get; set; }
-        public List<Task> TaskList { get; set; }
-    }
-}
