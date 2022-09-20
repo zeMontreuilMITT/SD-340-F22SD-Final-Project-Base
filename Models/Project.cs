e6e30d3ebf1f7370d6b45ce6b9a10c0b22a19c56
Fulin <944799613@qq.com>
2022-09-19 22:50:39 -0500

    finished DisplayName

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SD_340_W22SD_Final_Project_Group6.Models
 {
@@ -8,6 +9,7 @@ namespace SD_340_W22SD_Final_Project_Group6.Models
         [StringLength(200, ErrorMessage = "Project Name should be from 5 upto 200 characters only")]
         [MinLength(5)]
         [Required]
+        [DisplayName("Project Name :")]
         public string ProjectName { get; set; }
         public ApplicationUser? CreatedBy { get; set; }
         public ICollection<UserProject>? AssignedTo { get; set; } = new HashSet<UserProject>();
