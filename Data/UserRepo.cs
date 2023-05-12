5f45d99dca379fbc56d8487455bbfcf61634d27a
Reagan615 <419247451@qq.com>
2023-05-12 13:53:24 -0500

    test

--- Data/UserRepo.cs
+++ Data/UserRepo.cs
@@ -1,6 +1,44 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+﻿using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
 {
-    public class UserRepo
+    public class UserRepo : IRepository<ApplicationUser>
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ApplicationUser Create(ApplicationUser entity)
+        {
+            _context.Users.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public ApplicationUser Delete(ApplicationUser entity)
+        {
+            _context.Users.Remove(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public ApplicationUser? Get(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public ICollection<ApplicationUser> GetAll()
+        {
+            return 
+        }
+
+        public ApplicationUser Update(ApplicationUser entity)
+        {
+            _context.Users.Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
     }
 }
