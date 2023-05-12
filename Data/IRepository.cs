b7fd0d1472d21cb51ae6aa42f404fa517471423d
Paul Funston <paulfunston9@gmail.com>
2023-05-12 12:12:41 -0500

    Repository Interface Created

---
+++ Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        public T Create(T entity);
+
+        public T Get(int id);
+        public ICollection<T> GetAll();
+
+        public T Update(T entity);
+
+        public T Delete (T entity);
+    }
+}
