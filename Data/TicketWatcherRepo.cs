32fa91dfbad5a3917fcfe136bf66643c89dc15d3
Reagan615 <419247451@qq.com>
2023-05-12 13:34:39 -0500

    create ticketwatchRepo

--- Data/TicketWatcherRepo.cs
+++ Data/TicketWatcherRepo.cs
@@ -1,6 +1,44 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+﻿using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
 {
-    public class TicketWatcherRepo
+    public class TicketWatcherRepo : IRepository<TicketWatcher>
     {
+        private readonly ApplicationDbContext _context;
+
+        public TicketWatcherRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public TicketWatcher Create(TicketWatcher entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public TicketWatcher Delete(TicketWatcher entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public TicketWatcher Get(int id)
+        {
+            return _context.TicketWatchers.Find(id);
+        }
+
+        public ICollection<TicketWatcher> GetAll()
+        {
+            return _context.TicketWatchers.ToList();
+        }
+
+        public TicketWatcher Update(TicketWatcher entity)
+        {
+            _context.TicketWatchers.Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
     }
 }
