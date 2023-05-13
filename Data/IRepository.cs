7e842430b7432ca6ee93af32c3750ce8f9edc257
MattBanuag <matthewbanuag.dev@gmail.com>
2023-05-12 23:57:59 -0500

    Update: Initial set up by adding a generic interface

---
+++ Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        // CREATE
+        void Create(T entity);
+
+        // READ
+        T? Get(int? id);
+
+        // UPDATE
+        void Update(T entity);  
+
+        // DELETE
+        void Delete(int? id);
+    }
+}
