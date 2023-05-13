8c93776a4a0c1281f510e0bcc0e82097883a4d12
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-05-13 14:49:11 -0500

    Move all back end files

---
+++ JelloTicket.DataLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JelloTicket.DataLayer;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SD_340_W22SD_Final_Project_Group6.Models.Ticket> Tickets { get; set; }
+        public DbSet<SD_340_W22SD_Final_Project_Group6.Models.Project> Projects { get; set; }
+        public DbSet<SD_340_W22SD_Final_Project_Group6.Models.Comment> Comments { get; set; }
+        public DbSet<SD_340_W22SD_Final_Project_Group6.Models.UserProject> UserProjects { get; set; }
+        public DbSet<SD_340_W22SD_Final_Project_Group6.Models.TicketWatcher> TicketWatchers { get; set; }
+
+    }
+}
