076ac02e1c7e24028a275616cabd4d73b8955695
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-05-15 13:59:06 -0500

    Create business logic files

--- JelloTicket.BusinessLayer/Services/TicketBusinessLogic.cs
+++ JelloTicket.BusinessLayer/Services/TicketBusinessLogic.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using JelloTicket.DataLayer.Repositories;
-using JelloTicket.DataLayer.Models;
 
 namespace JelloTicket.BusinessLayer.Services
 {
-    public class ProjectBusinessLogic
+    internal class TicketBusinessLogic
     {
     }
 }
