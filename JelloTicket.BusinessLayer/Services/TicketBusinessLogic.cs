d5bbb39593e7d8458b9384360267e5d133c729d6
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-05-15 14:21:21 -0500

    update bll files

--- JelloTicket.BusinessLayer/Services/TicketBusinessLogic.cs
+++ JelloTicket.BusinessLayer/Services/TicketBusinessLogic.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JelloTicket.DataLayer.Repositories;
+using JelloTicket.DataLayer.Models;
 
 namespace JelloTicket.BusinessLayer.Services
 {
-    internal class TicketBusinessLogic
+    public class TicketBusinessLogic
     {
     }
 }
--- JelloTicket.BusinessLayer/Services/UserManagerBusinessLogic.cs
+++ JelloTicket.BusinessLayer/Services/UserManagerBusinessLogic.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using JelloTicket.DataLayer.Data;
+using JelloTicket.DataLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,15 @@ using System.Threading.Tasks;
 
 namespace JelloTicket.BusinessLayer.Services
 {
-    internal class UserManagerBusinessLogic
+    public class UserManagerBusinessLogic
     {
+        private readonly UserManager<ApplicationUser> _users;
+
+        // unsure if this is exactly how to inject
+        // might have to modify
+        public UserManagerBusinessLogic(UserManager<ApplicationUser> users)
+        {
+            _users = users;
+        }
     }
 }
