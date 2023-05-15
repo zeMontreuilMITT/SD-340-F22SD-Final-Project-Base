97a30372f407e469e366ce60825d23ae3f386fc6
Paul Funston <paulfunston9@gmail.com>
2023-05-15 15:19:57 -0500

    Implement helper method to GetDevelopers as a selectlistitem to be added to VMs

--- BLL/ProjectBL.cs
+++ BLL/ProjectBL.cs
@@ -1,20 +1,23 @@
 ﻿
 
+using Microsoft.AspNetCore.Identity;
 using SD_340_W22SD_Final_Project_Group6.Data;
 using SD_340_W22SD_Final_Project_Group6.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SD_340_W22SD_Final_Project_Group6.BLL
 {
     public class ProjectBL
     {
-        private IRepository<Project> _projectRepo;
-        private IRepository<Ticket> _ticketRepo;
-        private IRepository<Comment> _commentRepo;
-        private IRepository<TicketWatcher> _ticketWatcherRepo;
-        private IRepository<UserProject> _userProjectRepo;
-        private UserRepo _userRepo;
+        private readonly IRepository<Project> _projectRepo;
+        private readonly IRepository<Ticket> _ticketRepo;
+        private readonly IRepository<Comment> _commentRepo;
+        private readonly IRepository<TicketWatcher> _ticketWatcherRepo;
+        private readonly IRepository<UserProject> _userProjectRepo;
+        private readonly UserRepo _userRepo;
+        private readonly UserManager<ApplicationUser> _users;
 
-        public ProjectBL(IRepository<Project> projectRepo, IRepository<Ticket> ticketRepo, IRepository<Comment> commentRepo, IRepository<TicketWatcher> ticketWatcherRepo, IRepository<UserProject> userProjectRepo, UserRepo userRepo)
+        public ProjectBL(IRepository<Project> projectRepo, IRepository<Ticket> ticketRepo, IRepository<Comment> commentRepo, IRepository<TicketWatcher> ticketWatcherRepo, IRepository<UserProject> userProjectRepo, UserRepo userRepo, UserManager<ApplicationUser> users)
         {
             _projectRepo = projectRepo;
             _ticketRepo = ticketRepo;
@@ -22,6 +25,23 @@ namespace SD_340_W22SD_Final_Project_Group6.BLL
             _ticketWatcherRepo = ticketWatcherRepo;
             _userProjectRepo = userProjectRepo;
             _userRepo = userRepo;
+            _users = users;
+        }
+
+
+
+        public async Task<List<SelectListItem>> GetDevelopersAsSelectList()
+        {
+            List<ApplicationUser> allDevelopers = (List<ApplicationUser>)await _users.GetUsersInRoleAsync("Developer");
+
+            List<SelectListItem> developers = new();
+
+            allDevelopers.ForEach(developer =>
+            {
+                developers.Add(new SelectListItem(developer.UserName, developer.Id.ToString()));
+            });
+
+            return developers;
         }
 
     }
