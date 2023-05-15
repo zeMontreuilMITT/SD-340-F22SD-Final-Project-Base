e8058fbae9a54acf75be580e16eab7288bef0fb6
Reagan615 <419247451@qq.com>
2023-05-15 13:56:21 -0500

    create basic ticketBL

--- BLL/TicketBL.cs
+++ BLL/TicketBL.cs
@@ -1,6 +1,25 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.BLL
+﻿using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL
 {
     public class TicketBL
     {
+        private IRepository<Project> _projectRepo; 
+        private IRepository<Ticket> _ticketRepo;
+        private IRepository<Comment> _commentRepo;
+        private IRepository<TicketWatcher> _ticketWatcherRepo;
+        private IRepository<UserProject> _userProjectRepo;
+        private UserRepo _userRepo;
+
+        public TicketBL(IRepository<Project> projectRepo, IRepository<Ticket> ticketRepo, IRepository<Comment> commentRepo, IRepository<TicketWatcher> ticketWatcherRepo, IRepository<UserProject> userProjectRepo, UserRepo userRepo)
+        {
+            _projectRepo = projectRepo;
+            _ticketRepo = ticketRepo;
+            _commentRepo = commentRepo;
+            _ticketWatcherRepo = ticketWatcherRepo;
+            _userProjectRepo = userProjectRepo;
+            _userRepo = userRepo;
+        }
     }
 }
