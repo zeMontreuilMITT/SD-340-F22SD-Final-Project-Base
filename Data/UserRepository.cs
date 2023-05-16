7f61512139271e06fc23ff38da1e9635bfa9f3ce
MattBanuag <matthewbanuag.dev@gmail.com>
2023-05-15 22:01:04 -0500

    Create: UserRepository

---
+++ Data/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public class UserRepository : IRepository<ApplicationUser>
+    {
+        private UserManager<ApplicationUser> _users;
+
+        public UserRepository(UserManager<ApplicationUser> users)
+        {
+            _users = users;
+        }
+
+        void IRepository<ApplicationUser>.Create(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<ApplicationUser>.Delete(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser Get(string? id)
+        {
+            return _users.Users.First(u => u.Id == id);
+        }
+
+        ICollection<ApplicationUser> IRepository<ApplicationUser>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<ApplicationUser>.Update(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
