a07d4755891c5568e98e5811ede67e39ed279ba2
brendantyler <galmanbrendan@gmail.com>
2023-05-16 20:25:47 -0500

    Create Generic IRepository With Basic Crud

---
+++ Data/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        //Create
+        void Create(T entity);
+
+        //Read
+        T? Get(int? Id);
+        ICollection<T> GetAll();
+
+        //Update
+        void Update(T entity);
+
+        //Delete
+        void Delete(T entity);
+    }
+
+}
+
